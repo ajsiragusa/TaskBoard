4763d1c9cdef087cc3051a421d1ac5c149d24bf9
mgrantrev <dev@innov8tionhub.com>
2026-01-01 17:36:16 -0500

    Added Angular changes for Project routing and component structure: added Project component in app.html, added routing for Project and Issue components in app.routes.ts, and include Project in app.ts imports.

--- Angular-TaskBoard/src/app/app.ts
+++ Angular-TaskBoard/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Issue } from "./components/issue/issue";
+import { Project } from "./components/project/project";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Issue],
+  imports: [RouterOutlet, Issue, Project],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ Angular-TaskBoard/src/app/services/project-service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { ProjectData } from "../interfaces/project-data";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProjectService {
+    constructor(private httpClient: HttpClient) { }
+
+    getProjects() {
+        return this.httpClient.get<ProjectData[]>("http://localhost:8080/projects");
+    }
+
+    getProjectById(projectId: string) {
+        return this.httpClient.get<ProjectData>(`http://localhost:8080/projects/${projectId}`);
+    }
+
+    getProjectsByOwnerEmail(ownerEmail: string) {
+        return this.httpClient.get<ProjectData[]>(`http://localhost:8080/projects/owner/${ownerEmail}`);
+    }
+
+    postProject(projectName: string, projectDescription: string, projectOwnerEmail: string) {
+        const body = {
+            "name": projectName,
+            "description": projectDescription,
+            "owner": {
+                "email": projectOwnerEmail
+            }
+        }
+        return this.httpClient.post<ProjectData>(`http://localhost:8080/projects`, body);
+    }
+
+    updateProjectById(projectId: string, projectName: string, projectDescription: string, projectOwnerEmail: string) {
+        const body = {
+            "name": projectName,
+            "description": projectDescription,
+            "owner": {
+                "email": projectOwnerEmail
+            }
+        }
+        return this.httpClient.put<ProjectData>(`http://localhost:8080/projects/${projectId}`, body);
+    }
+
+    deleteProjectById(projectId: string) {
+        return this.httpClient.delete<ProjectData>(`http://localhost:8080/projects/${projectId}`);
+    }
+}
